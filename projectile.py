4db16c17893a0cd06724bf8e1eb3b339b2f0ed70
noxotlc <117919678+noxotlc@users.noreply.github.com>
2022-11-10 14:44:08 +0100

    Update projectile.py

--- projectile.py
+++ projectile.py
@@ -12,12 +12,20 @@ class Projectile:
         self.position = position # Attention ici position sera un tuple (x,y)
         self.vitesse = 5
         self.degat = 10
+        self.rect=self.image.get_rect()# donner les coordonné exacte du joueur ou essayer plusieur chiffre à x ou y pour que l'objet sorte de sa main
+        self.rect.x = joueur.rect.x +50
+        self.rect.y = joueur.rect.y +50
 
     def deplacement(self):
         (x, y) = self.position
         delta_x = 5
         delta_y = random.randint(- 5, 5)
         self.position = (x + delta_x, y + delta_y)
+         for ennemis in self.check_collision: #verifier si le, projectile rentre en collision avec un ennemis
+            self.remove()#supprimer le projectile
+            ennemis.damage(self.player.attack)# infliger des degats au monstre impacté par le projectile
+        if self.rect.x >800:
+            self.remove() #supprimer le projectile
 
     def affichage(self, surface):
         self.deplacement()
